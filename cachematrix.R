a8efae14bd6a3645bca26c63043e50a225b25ae0
ssfb1430 <shekhar25shah@gmail.com>
2023-03-11 17:19:54 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,22 @@
 #uses matrix created by user that allows user to get the matrix, set the values,
 #and apply inverse function on the matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+    #sets the matrix value
+      i <- NULL
+      set <- function(y){
+        x <<- y
+        i <<- NULL
+      }
+      #gets the matrix value
+      get <- function() x
+      #sets the inverse of the matrix
+      setInverse <- function(inverse) i <<- inverse
+      #gets the inverse of the matrix
+      getInverse <- function() i
+      #defines the matrix values (set, get, setInverse, getInverse) as list
+      list(set = get, get = get,
+           setInverse = setInverse
+           getInverse = getInverse)
 }
 
 
